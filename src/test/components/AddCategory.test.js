606a6bc0d134af5461fa7a3c3e6e209d3245c645
ansango <anibalsantosgo@gmail.com>
2021-08-17 15:46:26 +0200

    beforeeach mock functions

--- src/test/components/AddCategory.test.js
+++ src/test/components/AddCategory.test.js
@@ -2,14 +2,29 @@ import AddCategory from "../../components/AddCategory";
 const { shallow } = require("enzyme");
 
 describe("Test <AddCategory/>", () => {
-  const setCategories = () => {};
-  const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  const setCategories = jest.fn();
+  let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  });
+
   test("should render component", () => {
     expect(wrapper).toMatchSnapshot();
   });
+
   test("should change text box", () => {
     const input = wrapper.find("input");
     const value = "Hola Mundo";
+
     input.simulate("change", { target: { value } });
+  });
+
+  test("shouldn't submit value", () => {
+    wrapper.find("form").simulate("submit", {
+      preventDefault() {},
+    });
+    expect(setCategories).not.toHaveBeenCalled();
   });
 });
