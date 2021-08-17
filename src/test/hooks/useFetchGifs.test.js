ca407134654914cf2b7c2ac08c35b98b7c5dd003
ansango <anibalsantosgo@gmail.com>
2021-08-17 17:50:31 +0200

    finish testing

--- src/test/hooks/useFetchGifs.test.js
+++ src/test/hooks/useFetchGifs.test.js
@@ -2,11 +2,25 @@ import { renderHook } from "@testing-library/react-hooks";
 import useFetchGifs from "../../hooks/useFetchGifs";
 
 describe("test useFetchGigs", () => {
-  test("should return initial state", () => {
-    const category = " simpsons";
-    const { result } = renderHook(() => useFetchGifs(category));
+  const category = " simpsons";
+
+  test("should return initial state", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs(category)
+    );
     const { data, loading } = result.current;
-    expect(data).toEqual([]);
+    await waitForNextUpdate();
+    expect(data.length).toBe(0);
     expect(loading).toBe(true);
+  });
+
+  test("should return arr of imgs and false loading", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs(category)
+    );
+    await waitForNextUpdate();
+    const { data, loading } = result.current;
+    expect(data.length).toBe(12);
+    expect(loading).toBe(false);
   });
 });
